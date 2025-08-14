16fbaf708f83461f1d3123a8dbf8fec7025c9f47
Binh-2060 <binhxayxana@gmail.com>
2025-08-14 10:41:27 +0700

    main REF:: upgrade dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
-import { resolve } from "path";
 export default defineNuxtConfig({
   devtools: { enabled: false },
   ssr: false,
